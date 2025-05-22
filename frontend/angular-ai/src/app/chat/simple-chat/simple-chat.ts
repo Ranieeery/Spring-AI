6bf9903ae1b96f8ea0be3dbe36abef00c5e80d22
Ranieeery <raniery2003@hotmail.com>
2025-05-21 21:14:29 -0300

    feat: Add simple-chat component with routing and basic structure

---
+++ frontend/angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-chat',
+  imports: [],
+  templateUrl: './simple-chat.html',
+  styleUrl: './simple-chat.scss'
+})
+export class SimpleChat {
+
+}
