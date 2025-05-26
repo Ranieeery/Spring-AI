d1447e65466efb03614e874ea048a460cb4b4ee9
Ranieeery <raniery2003@hotmail.com>
2025-05-26 11:44:56 -0300

    feat: Enhance simple chat component with loading indicator and improved message handling

--- frontend/angular-ai/src/app/chat/simple-chat/simple-chat.ts
+++ frontend/angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
@@ -23,15 +23,20 @@ import { timeout } from 'rxjs';
   styleUrl: './simple-chat.scss',
 })
 export class SimpleChat {
+
+  @ViewChild('chatHistory')
+  private chatHistory!: ElementRef;
+
   userPrompt = '';
+  isLoading = false;
 
   messages = signal([{ text: 'Hello, how can I help you?', isBot: true }]);
-
   sendMessage() {
-    this.trimUserPrompt;
+    this.trimUserPrompt();
 
-    if (this.userPrompt.length !== 0) {
+    if (this.userPrompt.length !== 0 && !this.isLoading) {
       this.updateMessages(this.userPrompt);
+      this.isLoading = true;
       this.userPrompt = '';
       this.simulateBotResponse();
     }
@@ -40,18 +45,28 @@ export class SimpleChat {
   private trimUserPrompt() {
     this.userPrompt = this.userPrompt.trim();
   }
-
   private updateMessages(text: string, isBot = false) {
     this.messages.update((messages) => [
       ...messages,
-      { text: this.userPrompt, isBot: false },
+      { text: text, isBot: isBot },
     ]);
+    this.scrollToBottom();
   }
 
   private simulateBotResponse() {
     setTimeout(() => {
       const response = 'This is a simulated response from the bot.';
       this.updateMessages(response, true);
+      this.isLoading = false;
     }, 2000);
+  }
+
+  private scrollToBottom() {
+    try {
+      this.chatHistory.nativeElement.scrollTop =
+        this.chatHistory.nativeElement.scrollHeight;
+    } catch (error) {
+      console.error('Error scrolling to bottom:', error);
+    }
   }
 }
